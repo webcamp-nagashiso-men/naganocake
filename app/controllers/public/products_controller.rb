2ba730b22b3eedc0a41140a77cdabd0cfa8cb4a2
hirokoooomori <hirouta0408@gmail.com>
2021-08-17 14:29:57 +0000

    a

---
+++ app/controllers/public/products_controller.rb
@@ -0,0 +1,7 @@
+class Public::ProductsController < ApplicationController
+  def top
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,23 @@
 Rails.application.routes.draw do
-  
-  
-  
+
+
+
   devise_for :end_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   # 管理者側のルーティング設定
   namespace :admin do
     resources :products
   end
-  
+
   # 会員側のルーティング設定
   scope module: :public do
-    get 'top'=>'homes#top'
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+    get 'products' => 'products#index'
+    get 'products' => 'products#show'
     resources :product
   end
 
-  
+
 end
