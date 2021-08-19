dcdb69a7d35e6dbecee25ab96995304169671a2b
Genki1020 <wada1020dm@gmail.com>
2021-08-19 07:20:05 +0000

    +status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210819020530) do
+ActiveRecord::Schema.define(version: 20210819061624) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -24,26 +24,31 @@ ActiveRecord::Schema.define(version: 20210819020530) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "cart_products", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "end_user_id"
+    t.integer  "quantity"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "end_users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "remember_updated_at"
     t.string   "name_last"
     t.string   "name_first"
-
-
-　　　　　　　　t.string   "hurigana_last"
-   　　t.string   "hurigana_first"
-　　　　　　 t.string   "postal_code"
-
+    t.string   "hurigana_last"
+    t.string   "hurigana_first"
+    t.string   "postal_code"
     t.string   "address"
     t.string   "telephone_number"
-    t.boolean  "is_deleted"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "is_deleted",             default: false, null: false
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
