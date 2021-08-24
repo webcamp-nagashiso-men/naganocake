e544a9e82c2be114b56311883657f9692c51c9e0
hirokoooomori <hirouta0408@gmail.com>
2021-08-24 13:21:14 +0000

    genre

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,13 +1,26 @@
 class Public::ProductsController < ApplicationController
-    def top
-      @products = Product.all.order(created_at: :desc)
-      @genres = Genre.all
+  def top
+    @products = Product.all.order(created_at: :desc)
+    @genres = Genre.all
 	end
 
 	def index
-      @genres = Genre.all
-      @products = Product.search(params[:word]).page(params[:page]).per(8)
-      #@products = Product.all.page(params[:page]).per(8)
+	 # binding.pry
+    @genres = Genre.all
+      # @products = Product.all.page(params[:page]).per(8)
+      # #@products = Product.all.page(params[:page]).per(8)
+      # @products = Product.search(params[:word])
+
+    if params[:key_value]=="from_search"  #produts keyword search
+      @products = Product.where(['name LIKE ?', "%#{params[:word]}%"]).all.page(params[:page]).per(8)
+    elsif params[:search][:value] == "" # if genre null
+      @products = Product.all.page(params[:page]).per(8)
+      @null="null"
+    else # if genre present
+      @genre = Genre.find_by(name: params[:search][:value])
+      @products =@genre.products.page(params[:page]).per(8)
+      # binding.pry
+    end
 	end
 
 	def show
