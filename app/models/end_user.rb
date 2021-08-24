1a32fc836d9713cba5bbded85c11acd11d195d3e
toppo0323 <toppo9118@gmail.com>
2021-08-24 19:02:29 +0000

    css

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -10,4 +10,11 @@ class EndUser < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :cart_products, dependent: :destroy
 
+  validates :name_first, :name_last, :hurigana_first, :hurigana_last,
+            :address, :telephone_number,
+            presence: true
+  validates :postal_code, length: {is: 7}, numericality: { only_integer: true }
+  validates :telephone_number, numericality: { only_integer: true }
+ 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,7 @@ Rails.application.routes.draw do
   }
 
   devise_for :admins, skip: [:sessions]
-  devise_for :end_users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
 
   # 管理者側のルーティング設定
   namespace :admin do
