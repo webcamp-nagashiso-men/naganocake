2f28019084072e94efe50c2a8dd4882db98cc2f8
Genki1020 <wada1020dm@gmail.com>
2021-08-25 05:34:25 +0000

    syuusei

--- app/controllers/admin/order_products_controller.rb
+++ app/controllers/admin/order_products_controller.rb
@@ -2,8 +2,15 @@ class Admin::OrderProductsController < ApplicationController
 
     def update
     @order_product = OrderProduct.find(params[:id])
+    @order = @order_product.order
     @order_product.update(order_product_params)
-    redirect_to admin_order_path(@order_product.order_id)
+    if params[:order_product][:make_status] == "製作中"
+      @order.update(order_status: "製作中")
+    end
+    if @order.order_products.all?{ |order_product| order_product.make_status == "製作完了"}
+      @order.update(order_status: "発送準備中")
+    end
+     redirect_to admin_order_path(@order_product.order_id)
    end
 
   def order_product_params
