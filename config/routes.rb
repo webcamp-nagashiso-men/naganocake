fe5d874e90cb040116fd4266a9e2df210522ee97
MURATA-RYO <scincoidae@gmail.com>
2021-08-25 01:16:29 +0000

    prod8

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   }
 
   devise_for :admins, skip: [:sessions]
-  devise_for :end_users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
