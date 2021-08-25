4de6e09765c15adfdfdeff9ba443692f1ed92215
Genki1020 <wada1020dm@gmail.com>
2021-08-25 12:46:15 +0000

    syuusei

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -13,6 +13,11 @@ class Admin::OrdersController < ApplicationController
  def update
     @order = Order.find(params[:id])
     @order.update(order_params)
+       if @order.order_status == "入金確認"
+      @order.order_products.each do |order_product|
+        order_product.update(make_status: "製作待ち")
+      end
+    end
     redirect_to admin_order_path(@order.id)
  end
 
